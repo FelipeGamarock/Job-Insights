65a2ccfa78509533b0f3860f060dea1f111a588d
Felipe Gama Danta <epilefgama2@gmail.com>
2022-10-20 20:20:43 -0300

    feat: requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert 'title' in brazilian_jobs[0]
+    assert 'salary' in brazilian_jobs[0]
+    assert 'type' in brazilian_jobs[0]

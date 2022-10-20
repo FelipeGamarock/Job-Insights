05d94784e0dbfd0c8035721d67ffbdd2facbe3d6
Felipe Gama Danta <epilefgama2@gmail.com>
2022-10-20 20:23:00 -0300

    refactor: requisito 11, test for all jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,6 +3,7 @@ from src.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
     brazilian_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
-    assert 'title' in brazilian_jobs[0]
-    assert 'salary' in brazilian_jobs[0]
-    assert 'type' in brazilian_jobs[0]
+    for job in brazilian_jobs:
+        assert 'title' in job
+        assert 'salary' in job
+        assert 'type' in job

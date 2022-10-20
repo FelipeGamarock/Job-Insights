8792f277f49fd3e12e633f28db23ccf0fb7e8f77
Felipe Gama Danta <epilefgama2@gmail.com>
2022-10-20 17:11:01 -0300

    feat: requsito 3, before was req 2

--- src/insights.py
+++ src/insights.py
@@ -15,6 +15,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
+
+    jobs_list = read(path)
+    unique_industries = [job['industry']
+                         for job in jobs_list
+                         if job['industry']]
+    return set(unique_industries)
     """Checks all different industries and returns a list of them
 
     Must call `read`

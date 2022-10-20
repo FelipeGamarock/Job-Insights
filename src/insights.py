168cfed03a71288bb2e57127c9cd0485661e65cc
Felipe Gama Danta <epilefgama2@gmail.com>
2022-10-20 17:21:19 -0300

    feat: requisito 4

--- src/insights.py
+++ src/insights.py
@@ -21,21 +21,6 @@ def get_unique_industries(path):
                          for job in jobs_list
                          if job['industry']]
     return set(unique_industries)
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
 
 
 def filter_by_industry(jobs, industry):
@@ -57,21 +42,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    all_salaries = [int(job['max_salary'])
+                    for job in jobs_list
+                    if job["max_salary"].isnumeric()]
+    max_salary = max(all_salaries)
+    return max_salary
 
 
 def get_min_salary(path):
